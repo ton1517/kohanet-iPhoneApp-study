ffef672a79eccce7adec19b899387af71a7d9ee5
ton <tonton1517@gmail.com>
2013-06-07 18:57:36 +0900

    debug sample code

--- Tutorial/Tutorial/ViewController.m
+++ Tutorial/Tutorial/ViewController.m
@@ -14,10 +14,31 @@
 
 @implementation ViewController
 
+
+- (int)switchAnalyze:(int)num
+{
+    int value;
+    
+    switch (num) {
+        case 1:
+            value = 10;
+            break;
+        case 2:
+            value = 50;
+            break;
+        default:
+            break;
+    }
+    
+    return value;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self switchAnalyze:1];
 }
 
 - (void)didReceiveMemoryWarning
