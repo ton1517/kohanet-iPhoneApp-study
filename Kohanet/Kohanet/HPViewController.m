9f4f8eb027a93817ea19f632253d1f976c67eaa3
ton <tonton1517@gmail.com>
2013-06-08 04:15:45 +0900

    kohanetウェブサイトを表示

--- Kohanet/Kohanet/HPViewController.m
+++ Kohanet/Kohanet/HPViewController.m
@@ -9,15 +9,34 @@
 #import "HPViewController.h"
 
 @interface HPViewController ()
-
+{
+    NSURLRequest *request;
+}
 @end
 
 @implementation HPViewController
+
+static NSString * const HOME_URL = @"http://www.kohanet.jp/kodomokai/";
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    
+    if(self) {
+        
+        NSURL *url = [NSURL URLWithString:HOME_URL];
+        request = [NSURLRequest requestWithURL:url];
+    }
+    
+    return self;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self.webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning
